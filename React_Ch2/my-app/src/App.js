616ac30446be6512471e9cbd51f6a33703e7f10c
이영빈 <49426352+dudqls5271@users.noreply.github.com>
2022-04-13 17:57:04 +0900

    2022-04-13

--- React_Ch2/my-app/src/App.js
+++ React_Ch2/my-app/src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Composition from './compoments/2-4.Props/Composition';
+import Exataction from './compoments/2-4.Props/Exataction/Exataction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+      <Exataction/>
+      <Composition />
+  </div>
   );
 }
 
