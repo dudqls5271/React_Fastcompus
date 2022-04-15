40cecf7f7d5c640599661027e800db528c8566fb
이영빈 <49426352+dudqls5271@users.noreply.github.com>
2022-04-15 15:39:56 +0900

    State

--- React_Ch2/my-app/src/App.js
+++ React_Ch2/my-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Composition from './compoments/2-4.Props/Composition';
 import Exataction from './compoments/2-4.Props/Exataction/Exataction';
--- React_Ch2/my-app/src/compoments/2-5.State/FunctionalCommponent.jsx
+++ React_Ch2/my-app/src/compoments/2-5.State/FunctionalCommponent.jsx
@@ -20,7 +20,7 @@ export default function FunctionalCommponent() {
   return (
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      <h2>It is {date.toLocaleTimeString()}.</h2>
     </div>
   );
 }
