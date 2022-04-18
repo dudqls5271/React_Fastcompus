57a1a4db7e77b60244ee3e1cbf604edb392abbb0
YoungBin <49426352+dudqls5271@users.noreply.github.com>
2022-04-18 11:00:02 +0900

    Yarn

--- React_Ch2/my-app/src/App.js
+++ React_Ch2/my-app/src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import Composition from './compoments/2-4.Props/Composition';
 import Exataction from './compoments/2-4.Props/Exataction/Exataction';
 import ClassComposition from './compoments/2-5.State/ClassComposition';
+import ClassComponent from './compoments/2-6.LifeCycle/ClassComponent';
 import FunctionalCommponent from './compoments/2-5.State/FunctionalCommponent';
 
 function App() {
   return (
   <div className="App">
-      <FunctionalCommponent/>
-      <ClassComposition/>
-      <Exataction/>
-      <Composition />
+      <ClassComponent/>
+      {/* <FunctionalCommponent/> */}
+      {/* <ClassComposition/> */}
+      {/* <Exataction/> */}
+      {/* <Composition /> */}
   </div>
   );
 }
